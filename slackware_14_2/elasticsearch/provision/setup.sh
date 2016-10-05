8d6ed9fe44acfc9dce74d1d41cfcbd2dfef0db79
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-05 08:38:05 -0400

    Reconfiguring the layout of provisioning scripts
    With tips from https://www.sitepoint.com/vagrantfile-explained-setting-provisioning-shell/

---
+++ slackware_14_2/elasticsearch/provision/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+
+echo "Downloading Elasticsearch..."
+wget --quiet https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.1/elasticsearch-2.4.1.tar.gz
+echo "Downloading Elasticsearch...DONE"
+
+echo "Extracting Elasticsearch..."
+tar xzf elasticsearch-2.4.1.tar.gz
+echo "Extracting Elasticsearch...DONE"
+
+echo "Configuring Elasticsearch..."
+chown -R vagrant:users elasticsearch-2.4.1
+rm 	-f elasticsearch-2.4.1.tar.gz
+mkdir opt
+mv elasticsearch-2.4.1 opt
+ln -s opt/elasticsearch-2.4.1/bin/elasticsearch
+echo "Configuring Elasticsearch...DONE"
