07e853518c3da9788dc7e34be150f2b1c53c0c2e
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-05 16:18:15 -0400

    Add a build for a doormouse server

---
+++ slackware_14_2/doormouse/provision/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+wget --quiet https://pypi.python.org/packages/e7/a8/7556133689add8d1a54c0b14aeff0acb03c64707ce100ecd53934da1aa13/pip-8.1.2.tar.gz
+wget --quiet https://slackbuilds.org/slackbuilds/14.2/python/pip.tar.gz
+tar xzf pip.tar.gz 
+mv pip-8.1.2.tar.gz pip
+pushd pip
+./pip.SlackBuild 
+installpkg --terse /tmp/pip-8.1.2-i586-1_SBo.tgz 
+popd
+rm -rf /tmp/pip-8.1.2-i586-1_SBo.tgz pip*
+pip install doormouse
+pip install django 
+wget --quiet fastdl.mongodb.org/linux/mongodb-linux-i686-3.2.10.tgz
+tar xzf mongodb-linux-i686-3.2.10.tgz 
+pushd mongodb-linux-i686-3.2.10
+mkdir -p /data/db/
+chown vagrant /data/db 
+bin/mongod --storageEngine=mmapv1 --fork --logpath /var/log/monogd.log
+popd
+pip install pymongo
+wget --quiet https://doormouse.io/doormouse-platform.tar.gz
+tar xzf doormouse-platform.tar.gz 
+pushd doormouse-platform/src/
+python manage.py migrate
+popd
+echo "Still need to manually do the following:"
+echo ""
+echo "pushd doormouse-platform/src/"
+echo "python manage.py createsuperuser"
+echo "python manage.py runserver 0.0.0.0:80"
+echo ""
