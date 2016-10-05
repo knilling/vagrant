2afc932aed2e777110c64be060476ce69017f0e6
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-05 12:49:35 -0400

    Fix a typo

--- slackware_14_2/bro/provision/setup.sh
+++ slackware_14_2/bro/provision/setup.sh
@@ -3,7 +3,7 @@
 echo "Provisioning virtual machine..."
 
 echo "Downloading Bro Slackware Package..."
-wget --quiet https://www.dropbox.com/s/ams81e87juwiyx1/bro-2.4.1-i386-1.tgz?dl=0
+wget --quiet https://www.dropbox.com/s/ams81e87juwiyx1/bro-2.4.1-i386-1.tgz
 echo "Downloading Bro Slackware Package...DONE"
 
 echo "Installing Bro Slackware Package..."
