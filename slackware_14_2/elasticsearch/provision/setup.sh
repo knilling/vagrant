fe7bdcaaca0a7349974f05b0775d3e7cd8b9b137
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-05 09:09:35 -0400

    Automatically modify elasticsearch config to use public IP

--- slackware_14_2/elasticsearch/provision/setup.sh
+++ slackware_14_2/elasticsearch/provision/setup.sh
@@ -12,8 +12,16 @@ echo "Extracting Elasticsearch...DONE"
 
 echo "Configuring Elasticsearch..."
 chown -R vagrant:users elasticsearch-2.4.1
-rm 	-f elasticsearch-2.4.1.tar.gz
+rm -f elasticsearch-2.4.1.tar.gz
+IP_ADDRESS=$(/sbin/ip -f inet addr show eth1 | grep -Po 'inet \K[\d.]+')
+
+echo "network.host: $IP_ADDRESS" >> elasticsearch-2.4.1/config/elasticsearch.yml
+
 mkdir opt
 mv elasticsearch-2.4.1 opt
+
 ln -s opt/elasticsearch-2.4.1/bin/elasticsearch
 echo "Configuring Elasticsearch...DONE"
+echo ""
+echo "IP Address: $IP_ADDRESS"
+echo ""
