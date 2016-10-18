5ab5c428f18e2d2ab65cf278da8252377f2a3935
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-18 14:21:58 -0400

    Install docker-compose
    https://github.com/docker/compose/releases/tag/1.8.1

--- individual_machines/slackware_14_2/docker/provision/setup.sh
+++ individual_machines/slackware_14_2/docker/provision/setup.sh
@@ -7,6 +7,8 @@ wget --quiet https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
 tar xzf docker-latest.tgz
 sudo mv docker/* /usr/local/bin/
 rm -rf docker docker-latest.tgz 
+curl -s -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 wget --quiet https://raw.githubusercontent.com/knilling/slackware/master/etc/rc.d/rc.docker -O /etc/rc.d/rc.docker
 chmod +x /etc/rc.d/rc.docker
 /etc/rc.d/rc.docker start
