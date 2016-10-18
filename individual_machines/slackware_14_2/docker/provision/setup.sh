7fa6df4ba2dde1dc2b9aeb345c90c996be620bfe
Chris Crawford <christopher.p.crawford@gmail.com>
2016-10-18 14:21:28 -0400

    Silently extract docker

--- individual_machines/slackware_14_2/docker/provision/setup.sh
+++ individual_machines/slackware_14_2/docker/provision/setup.sh
@@ -4,7 +4,7 @@ sed -i 's/^# Defaults secure/Defaults secure/' /etc/sudoers
 groupadd docker
 usermod -G docker vagrant
 wget --quiet https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
-tar xvzf docker-latest.tgz
+tar xzf docker-latest.tgz
 sudo mv docker/* /usr/local/bin/
 rm -rf docker docker-latest.tgz 
 wget --quiet https://raw.githubusercontent.com/knilling/slackware/master/etc/rc.d/rc.docker -O /etc/rc.d/rc.docker
